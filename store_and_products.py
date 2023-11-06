4c115173e686838dc3aef112c6cdb72630f93e45
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-11-06 15:41:11 -0600

    I added add_product() to Store class and print_info()

--- store_and_products.py
+++ store_and_products.py
@@ -4,6 +4,21 @@ class Store:
         self.name = name
         self.products = []
 
+# Let's also give some methods to our Store class:
+
+# add_product(self, new_product) - takes a product and adds it to the store
+    def add_product(self, new_product):
+        if new_product not in self.products:
+            self.products.append(new_product)
+        else:
+            print(f"The store: {self.name} already has a product named {new_product}")
+        return self
+    
+    def print_info(self):
+        print(f"Your store {self.name} has {len(self.products)} product(s): {self.products}")
+        return self
+
+
 # Next, create a Product class that has 3 attributes: a name, a price, and a category. All of these should be provided upon creation.
 class Product:
     def __init__(self, name, price, category):
@@ -24,6 +39,11 @@ class Product:
     def print_info(self):
         print(f"Name: {self.name}\nPrice: ${self.price}\nCategory: {self.category}")
         return self
+    
+s_1 = Store("White Night")
+
+s_1.add_product("Ramy").print_info().add_product("Domino").print_info()
 
 p_1 = Product("Bino", 49.99, "Board Game")
+
 p_1.print_info().update_price(15, True).print_info()
